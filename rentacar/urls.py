a1df5f8eaa28cfa21591959953a77c51ddd2e7a0
Asmodeus0613 <ayadekenneth07@gmail.com>
2023-10-11 23:22:00 +0800

    origin

--- rentacar/urls.py
+++ rentacar/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('home', views.home, name='home'),
     path('cars/', views.cars, name='cars'),
     path('about/', views.about, name='about'),
-    path('login/', views.login_view, name='login'),
+    path('login/', LoginView.as_view(template_name='RentACar/login.html'), name='login'),
 ]
