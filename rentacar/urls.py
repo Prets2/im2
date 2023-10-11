67d0c4811e294e95ba3f62df5bf4efc45a6e1cfb
Asmodeus0613 <ayadekenneth07@gmail.com>
2023-10-11 22:53:23 +0800

    origin

--- rentacar/urls.py
+++ rentacar/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.landing_page, name='landing_page'),
+    path('home', views.home, name='home'),
     path('cars/', views.cars, name='cars'),
     path('about/', views.about, name='about'),
+    path('login/', views.login_view, name='login'),
 ]
--- rentacar/views.py
+++ rentacar/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
+
+def landing_page(request):
+    return render(request, 'RentACar/landingpage.html')
 
 def home(request):
     return render(request, 'RentACar/homepage.html')
@@ -9,3 +14,17 @@ def cars(request):
 
 def about(request):
     return render(request, 'RentACar/about.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page or another page
+            return redirect('home')
+        else:
+            # Return an error message or handle the failed login attempt
+            pass
+    return render(request, 'login.html')
