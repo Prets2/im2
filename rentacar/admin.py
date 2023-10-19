ede952cf85fc1dd1400aa652b0ea5368f195c820
daestortion <danielgilbertd@gmail.com>
2023-10-19 16:29:57 +0800

    origin

--- rentacar/admin.py
+++ rentacar/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['CarID', 'carName', 'carType', 'carDescription', 'carRate', 'display_car_pic']
+
+    def display_car_pic(self, obj):
+        if obj.carPic:
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 100px;" />', obj.carPic.url)
+        else:
+            return "No Image"
+
+    display_car_pic.short_description = 'Car Picture'
+
+admin.site.register(Car, CarAdmin)
--- rentacar/models.py
+++ rentacar/models.py
@@ -1,2 +1,14 @@
 from django.contrib.auth.models import BaseUserManager, AbstractUser
 
+from django.db import models
+
+class Car(models.Model):
+    CarID = models.AutoField(primary_key=True)
+    carName = models.CharField(max_length=255)
+    carType = models.CharField(max_length=100)
+    carDescription = models.TextField()
+    carRate = models.DecimalField(max_digits=10, decimal_places=2)
+    carPic = models.ImageField(upload_to='car_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.carName
