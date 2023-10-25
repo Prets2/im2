15ef94ac88fd5c48e6833210bb8fb0b2cec849d0
Asmodeus0613 <ayadekenneth07@gmail.com>
2023-10-25 22:13:29 +0800

    origin

--- rentacar/urls.py
+++ rentacar/urls.py
@@ -5,8 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
-
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('home', views.home, name='home'),
