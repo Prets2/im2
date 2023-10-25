7c3afabcd14d6a2c3d712376f5c5ba040cbec27a
daestortion <danielgilbertd@gmail.com>
2023-10-26 00:20:14 +0800

    origin

--- rentacar/forms.py
+++ rentacar/forms.py
@@ -2,16 +2,8 @@
 
 from django import forms
 from .models import Car
-<<<<<<< HEAD
-=======
-from .models import Order
->>>>>>> 7f806860f1e67defe72020e07a1eb3a4ea72fa32
 
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = ['carName', 'carType', 'carDescription', 'carRate']
-<<<<<<< HEAD
-=======
-
->>>>>>> 7f806860f1e67defe72020e07a1eb3a4ea72fa32
